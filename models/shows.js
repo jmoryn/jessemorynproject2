8400289d0cac180e3741afe25f9601cbba4515ba
Jesse Moryn <macuser@Macs-MacBook-Pro.local>
2021-04-28 14:42:18 -0500

    rename model and controller to shows and create shows model

---
+++ models/shows.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const showSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    location: {type: String, required: true},
+    time: {type: String, required: true},
+    price: {type: Number, min: 0, required: true},
+    description: String,
+    image: String
+})
+
+const Show = mongoose.model('Show', showSchema)
+
+module.exports = Show;
